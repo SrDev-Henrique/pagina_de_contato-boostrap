ee49c9b5841abe54ef6e2b1cc5cdf99a7a9f3e99
Henrique Albuquerque <halbuquerque2850@gmail.com>
2024-10-13 18:04:25 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// todo: Seleciona os elementos para animar
+const elements = document.querySelectorAll('.h4, .p, .a, .img');
+
+// todo: Função para adicionar animação quando o elemento estiver visível
+const observer = new IntersectionObserver((entries) => {
+entries.forEach(entry => {
+    if (entry.isIntersecting) {
+    entry.target.classList.add('visible');
+    }
+});
+}, { threshold: 1 });
+
+// todo: Observa cada elemento individualmente
+elements.forEach(element => {
+observer.observe(element);
+});
+
+$(document).ready(function () {
+
+    $('.coll').click(function () {
+        $('.nav-menu').slideToggle();
+    })
+})
